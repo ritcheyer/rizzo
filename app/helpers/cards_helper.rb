546bfb28660c7822c67ec0c1e10aeba7c8791190
Matt Hinchliffe <matt@maketea.co.uk>
2014-08-19 17:00:01 +0100

    Make the cards helper spec a little more stringent

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -10,7 +10,8 @@ module CardsHelper
       "card--#{props[:cover?] ? 'cover' : 'standard'}",
       "card--#{props[:double?] ? 'double' : 'single'}",
       "card--#{props[:image_url] ? 'has-img' : 'no-img'}",
-      "card--#{props[:meta_description] || props[:meta_author] || props[:image_attribution] ? 'has-meta' : 'no-meta'}"
+      "card--#{props[:image_attribution] ? 'has-attribution' : 'no-attribution'}",
+      "card--#{props[:meta_description] || props[:meta_author] ? 'has-meta' : 'no-meta'}"
     ]
   end
 
--- spec/helpers/cards_helper_spec.rb
+++ spec/helpers/cards_helper_spec.rb
@@ -26,14 +26,32 @@ describe CardsHelper do
       result.should include("card--fixed")
     end
 
-    it "returns an array of content class names for the given properties" do
-      result = helper.card_classes(
-        image_url: "path/to/image",
-        meta_description: "Meta content"
-      )
+    describe "returns an array of content class names for the given properties" do
 
-      result.should include("card--has-img")
-      result.should include("card--has-meta")
+      it "should add 'has' class names when given content" do
+        result = helper.card_classes(
+          image_url: "path/to/image",
+          meta_description: "Meta content",
+          image_attribution: "This photo was taken by Joe Bloggs"
+        )
+
+        result.should include("card--has-img")
+        result.should include("card--has-meta")
+        result.should include("card--has-attribution")
+      end
+
+      it "should add 'no' class names when given no content" do
+        result = helper.card_classes(
+          image_url: nil,
+          meta_description: nil,
+          image_attribution: nil
+        )
+
+        result.should include("card--no-img")
+        result.should include("card--no-meta")
+        result.should include("card--no-attribution")
+      end
+
     end
 
   end
