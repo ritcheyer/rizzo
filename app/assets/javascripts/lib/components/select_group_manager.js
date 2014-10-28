8fdf7c8edce30d51b3f56f159ca9bedc6022245d
Matt Hinchliffe <matt@maketea.co.uk>
2014-10-28 12:32:51 +0000

    Translate select manager spec CS -> JS and DRY up the select manager a bit

--- app/assets/javascripts/lib/components/select_group_manager.js
+++ app/assets/javascripts/lib/components/select_group_manager.js
@@ -14,40 +14,41 @@ define([ "jquery" ], function($) {
   SelectGroupManager.prototype.init = function() {
     var _this = this;
 
-    this.$selectContainers.on("focus", ".js-select", function(e) {
-      _this._getOverlay(e.target).addClass("is-selected");
-    });
+    this.$selectContainers
+      .on("focus", ".js-select", function(e) {
+        _this._getOverlay($(e.target)).addClass("is-selected");
+      })
+      .on("blur", ".js-select", function(e) {
+        _this._getOverlay($(e.target)).removeClass("is-selected");
+      })
+      .on("keyup", ".js-select", function(e) {
+        $(e.target).trigger("change");
+      })
+      .on("change", ".js-select", function(e) {
+        var $target = $(e.target);
 
-    this.$selectContainers.on("blur", ".js-select", function(e) {
-      _this._getOverlay(e.target).removeClass("is-selected");
-    });
+        e.preventDefault();
 
-    this.$selectContainers.on("keyup", ".js-select", function(e) {
-      $(e.target).trigger("change");
-    });
+        _this._updateOverlay($target);
 
-    this.$selectContainers.on("change", ".js-select", function(e) {
-      e.preventDefault();
-      _this._updateOverlay(e.target);
-
-      if ($(e.target).data("form-submit")) {
-        _this._submit(e.target);
-      }
-    });
+        if ($target.data("form-submit")) {
+          _this._submit($target);
+        }
+      });
   };
 
-  SelectGroupManager.prototype._getOverlay = function(target) {
-    return $(target).closest(".js-select-group-manager").find(".js-select-overlay");
+  SelectGroupManager.prototype._getOverlay = function($target) {
+    return $target.closest(".js-select-group-manager").find(".js-select-overlay");
   };
 
-  SelectGroupManager.prototype._updateOverlay = function(target) {
-    var t = $(target).find("option:selected");
-    this._getOverlay(target).text(t.text());
+  SelectGroupManager.prototype._updateOverlay = function($target) {
+    var $option = $target.find("option:selected");
+    this._getOverlay($target).text($option.text());
   };
 
-  SelectGroupManager.prototype._submit = function(target) {
-    if ($(target).val() !== "") {
-      $(target).closest("form").submit();
+  SelectGroupManager.prototype._submit = function($target) {
+    if ($target.val()) {
+      $target.closest("form").submit();
     }
   };
 
