38456aaf26b2a09476f84c786259579ff7205d11
Matt Hinchliffe <matt@maketea.co.uk>
2014-11-05 17:05:01 +0000

    Update card grid helper to account for ad rail and remove columns argument

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -77,31 +77,46 @@ module CardsHelper
     end
   end
 
-  def card_grid_helper(card_index: 0, is_double: false, is_mpu: false, grid_columns: 5, reset: false)
-    class_str = ['col--one-whole']
+  def card_grid_classes(is_double: false, has_rail: false)
+    class_names = ['col--one-whole']
+
+    if is_double
+      class_names += ['nv--col--one-whole', 'mv--col--two-thirds', 'lv--col--one-half']
+
+      if has_rail
+        class_names += ['wv--col--two-thirds', 'cv--col--one-half']
+      else
+        class_names += ['wv--col--two-fifths', 'cv--col--one-third']
+      end
+    else
+      class_names += ['nv--col--one-half', 'mv--col--one-third', 'lv--col--one-quarter']
+
+      if has_rail
+        class_names += ['wv--col--one-third', 'cv--col--one-quarter']
+      else
+        class_names += ['wv--col--one-fifth', 'cv--col--one-sixth']
+      end
+    end
+
+    class_names
+  end
+
+  def card_grid_helper(card_index: 0, is_double: false, is_mpu: false, has_rail: false, reset: false)
+    class_str = card_grid_classes(is_double: is_double || is_mpu, has_rail: has_rail)
 
     @grid_helper_doubles = 0 if card_index == 0 || reset
     @grid_helper_doubles += 1 if is_double
 
-    if is_double || is_mpu
-      class_str << 'col--right' if is_mpu
-      class_str << 'mv--col--two-thirds' if grid_columns >= 3
-      class_str << 'lv--col--one-half' if grid_columns >= 4
-      class_str << 'wv--col--two-fifths' if grid_columns >= 5
-    else
-      class_str << 'nv--col--one-half' if grid_columns >= 2
-      class_str << 'mv--col--one-third' if grid_columns >= 3
-      class_str << 'lv--col--one-quarter' if grid_columns >= 4
-      class_str << 'wv--col--one-fifth' if grid_columns >= 5
-    end
+    class_str << 'col--right' if is_mpu
 
     position_index = card_index + @grid_helper_doubles
 
     if card_index > 0
-      class_str << 'nv--clear' if position_index % 2 == 0 && grid_columns >= 2
-      class_str << 'mv--clear' if position_index % 3 == 0 && grid_columns >= 3
-      class_str << 'lv--clear' if position_index % 4 == 0 && grid_columns >= 4
-      class_str << 'wv--clear' if position_index % 5 == 0 && grid_columns >= 5
+      class_str << 'nv--clear' if position_index % 2 == 0
+      class_str << 'mv--clear' if position_index % 3 == 0
+      class_str << 'lv--clear' if position_index % 4 == 0
+      class_str << 'wv--clear' if (has_rail && position_index % 3 == 0) || (!has_rail && position_index % 5 == 0)
+      class_str << 'cv--clear' if (has_rail && position_index % 4 == 0) || (!has_rail && position_index % 6 == 0)
     end
 
     class_str
