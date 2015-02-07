273c526cc7b44084783c2f57f0ada150c0d4211a
chee <chee@snaek.org>
2015-02-07 15:05:37 +0000

    add bbqdeparam to spechelper

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -18,6 +18,7 @@ require.config({
   paths: {
     picker: "vendor/assets/javascripts/pickadate/lib/picker",
     pickerDate: "vendor/assets/javascripts/pickadate/lib/picker.date",
-    pickerLegacy: "vendor/assets/javascripts/pickadate/lib/legacy"
+    pickerLegacy: "vendor/assets/javascripts/pickadate/lib/legacy",
+    "jquery-bbq-deparam/jquery-deparam": "vendor/assets/javascripts/jquery-bbq-deparam/jquery-deparam"
   }
 });
