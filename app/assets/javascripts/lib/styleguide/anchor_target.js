1f3636c57fdf029ffa2550d319f21a1be8541262
chee <chee@snaek.org>
2015-02-22 04:41:26 +0000

    add anchor target js for styleguide

---
+++ app/assets/javascripts/lib/styleguide/anchor_target.js
@@ -0,0 +1,17 @@
+define([ "jquery" ], function($) {
+  "use strict";
+
+  var hash = location.hash.substring(1),
+      $anchorTarget = $(".js-anchor-target");
+
+  if (!$anchorTarget.length) return;
+
+  if (hash.length) {
+    $anchorTarget.val(hash);
+    $anchorTarget.trigger("input");
+  }
+
+  $anchorTarget.on("input", function() {
+    window.history.replaceState(null, null, "#" + this.value);
+  });
+});
