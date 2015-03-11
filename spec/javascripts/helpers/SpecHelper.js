d2945c5b571dbee71398bd0e99272abce807b2ff
Matt Hinchliffe <matt@maketea.co.uk>
2015-03-11 09:16:33 +0000

    Move RequireJS config to Gruntfile, load jQuery after RequireJS

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -13,12 +13,3 @@ window.lp = {
   isMobile: false,
   getCookie: function() {},
 };
-
-require.config({
-  paths: {
-    picker: "vendor/assets/javascripts/pickadate/lib/picker",
-    pickerDate: "vendor/assets/javascripts/pickadate/lib/picker.date",
-    pickerLegacy: "vendor/assets/javascripts/pickadate/lib/legacy",
-    "jquery-bbq-deparam/jquery-deparam": "vendor/assets/javascripts/jquery-bbq-deparam/jquery-deparam"
-  }
-});
